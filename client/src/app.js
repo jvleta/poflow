fade7ba250c909477ae42289eec8ddc035664404
Jimmy Leta <jimmy.leta@gmail.com>
2021-11-21 07:58:04 -0500

    commenting out reference to firestore

--- client/src/app.js
+++ client/src/app.js
@@ -31,7 +31,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore();
+// const db = getFirestore();
 export const analytics = getAnalytics(app);
 
 // try {
