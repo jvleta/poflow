71f668b5215b20f3db621891473444844d3f6be9
Jimmy Leta <jimmy.leta@gmail.com>
2021-11-21 19:56:05 -0500

    inital commit of google tests

--- wasm/main.cpp
+++ wasm/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 
+#include "gtest/gtest.h"
+
 #include "geometry.h"
 
-int main() {
-    constexpr int num_points = 1000;
-    std::cout << poflow::get_naca00XX_coords(num_points, 0.5);
+
+TEST(EllipseGeometryTests, TwoToOneEllipse) {
+  constexpr int num_points = 100;
+  constexpr double ratio = 0.50;
+  constexpr char filename[] = "ellipse21.txt";
+  std::cout << poflow::get_ellipse_coords(num_points, ratio);
+}
+
+TEST(Naca00XXCoordinateTests, Naca0006) {
+  constexpr int num_points = 100;
+  constexpr double ratio = 0.06;
+  constexpr char filename[] = "naca0006.txt";
+  std::cout << poflow::get_naca00XX_coords(num_points, ratio);
+}
+
+TEST(Naca00XXCoordinateTests, Naca0008) {
+  constexpr int num_points = 100;
+  constexpr double ratio = 0.08;
+  constexpr char filename[] = "naca0008.txt";
+  std::cout << poflow::get_naca00XX_coords(num_points, ratio);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
