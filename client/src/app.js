2966fd96c347e63407e1b94df83bbee2da3a2eca
Jimmy Leta <jimmy.leta@gmail.com>
2021-11-25 19:35:48 -0500

    initial commit of multi-step form implementation

--- client/src/app.js
+++ client/src/app.js
@@ -1,30 +1,135 @@
-import { createApp } from 'https://unpkg.com/petite-vue?module';
+import { createApp, nextTick } from 'https://unpkg.com/petite-vue?module';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.4.1/firebase-analytics.js';
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js';
 import { addDoc, collection, getFirestore } from 'https://www.gstatic.com/firebasejs/9.4.1/firebase-firestore.js'
 
 import { firebaseConfig } from './config.js';
 import { GetEllipseCoordinates, GetNaca00XXCoordinates } from './poflow.js'
+import { autocomplete } from './autocomplete.js';
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 export const analytics = getAnalytics(app);
 
-// petitevue
+function FieldComponent(props) {
+    return {
+        $template: "#field-component-template",
+        field: props.field,
+        get isInvalid() {
+            return props.isInvalid();
+        },
+        get invalidMessage() {
+            return props.invalidMessage();
+        },
+        // methods
+        validate() {
+            nextTick(() => {
+                if (this.isInvalid) props.validate();
+            });
+        }
+    };
+}
+
+function StepsIndicatorComponent(props) {
+    return {
+        $template: "#step-indicator-component-template",
+        stepsCount: props.stepsCount,
+        get stepsCountWithSuccessPage() {
+            return this.stepsCount + 1;
+        }
+    };
+}
+
 
 createApp({
-    // Data
+    StepsIndicatorComponent,
+    FieldComponent,
     currentStep: 0,
     submitted: false,
     invalids: {},
     fields: {
         airfoiltype: {
+            id: 'myInput',
             label: 'Airfoil Type',
             value: '',
             validations: [{ message: 'Name is a required field', test: (value) => value }]
         },
+        horizontalVelocity: {
+            id: '',
+            label: 'Horizontal Velocity',
+            value: '',
+            validations: []
+        },
+        verticalVelocity: {
+            id: '',
+            label: 'Vertical Velocity',
+            value: '',
+            validations: []
+        }
+    },
+    steps: [
+        ['airfoiltype'],
+        ['horizontalVelocity', 'verticalVelocity']
+    ],
+    get currentFields() {
+        return this.steps[this.currentStep];
+    },
+    get totalSteps() {
+        return this.steps.length;
+    },
+    get isFirstStep() {
+        return this.currentStep === 0;
+    },
+    get isLastStep() {
+        return this.currentStep === this.totalSteps - 1;
+    },
+
+    // Methods
+    previousStep() {
+        if (this.isFirstStep) return;
+        // removes all invalids so doesn't show error messages on back
+        this.invalids = {};
+        this.currentStep--;
+    },
+    nextStep() {
+        if (this.isLastStep) return;
+        this.validate();
+        if (this.isInvalid) return;
+        this.currentStep++;
+    },
+    get isInvalid() {
+        return !!Object.values(this.invalids).filter((key) => key).length;
+    },
+    // methods
+    validate() {
+        this.invalids = {};
+        // validates all the fields on the current page
+        this.currentFields.forEach((key) => {
+            this.validateField(key);
+        });
+    },
+    validateField(fieldKey) {
+        this.invalids[fieldKey] = false;
+        const field = this.fields[fieldKey];
+        // run through each of the fields validation tests
+        field.validations.forEach((validation) => {
+            if (!validation.test(field.value)) {
+                this.invalids[fieldKey] = validation.message;
+            }
+        });
+    },
+
+    submit() {
+        // if form not valid don't submit
+        this.validate();
+        if (this.isInvalid) return;
+
+        // submit on valid form
+        console.log("doing submit", this.fields);
+        this.submitted = true;
     }
+
 }).mount('#multi-step-form');
 
 
@@ -73,13 +178,33 @@ const config = {
         pointRadius: 0,
         scales: {
             x: {
-                title: { font: { size: 24 }, display: true, text: 'adfadsfdsafa' },
+                title: { font: { size: 24 }, display: true, text: 'x/L' },
                 type: 'linear',
-                position: 'bottom'
+                position: 'bottom',
+                ticks: {
+                    font: {
+                        size: 22,
+                    }
+                },
+            },
+            y: {
+                type: 'linear',
+                ticks: {
+                    font: {
+                        size: 22,
+                    }
+                },
             }
         },
         plugins: { legend: { display: false } }
     }
 
 };
+
+// const airfoils = [
+//     'NACA-0006', 'NACA-0008', 'NACA-0010', 'NACA-0015', 'NACA-0018', 'NACA-0021',
+//     'NACA-0024'
+// ];
+
+// autocomplete(document.getElementById('myInput'), airfoils);
 const myChart = new Chart(document.getElementById('myChart'), config);
