98040eaadfb2d9e3b6162fcf42549b72a9f4ae88
Jimmy Leta <jimmy.leta@gmail.com>
2021-11-27 22:19:32 -0500

    refactoring

--- client/src/poflow.js
+++
@@ -1,5 +0,0 @@
-export const GetEllipseCoordinates =
-    Module.cwrap('GetEllipseCoordinates', 'string', ['number', 'number']);
-
-export const GetNaca00XXCoordinates =
-    Module.cwrap('GetNaca00XXCoordinates', 'string', ['number', 'number']);
