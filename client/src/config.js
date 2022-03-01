821830918b0726870084c318828cf48e8730e196
Jimmy Leta <jimmy.leta@gmail.com>
2022-02-28 20:00:13 -0500

    Commenting out content for now

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,10 @@
-export const firebaseConfig = {
-    apiKey: 'AIzaSyA7oxGdbv2i-fqEdNYG2D_fCpyfP96-uE0',
-    authDomain: 'poflow-leta-tech.firebaseapp.com',
-    projectId: 'poflow-leta-tech',
-    storageBucket: 'poflow-leta-tech.appspot.com',
-    messagingSenderId: '545726653232',
-    appId: '1:545726653232:web:b2b8a27aba7f00235c9ef5',
-    measurementId: 'G-GSENSHJ9BM'
+const firebaseConfig = {
+  apiKey: "AIzaSyAxqZ8gFNSVmQjhaUPbSdzGAytXWu7LrVo",
+  authDomain: "poflow-app.firebaseapp.com",
+  projectId: "poflow-app",
+  storageBucket: "poflow-app.appspot.com",
+  messagingSenderId: "976282440160",
+  appId: "1:976282440160:web:54faaf2845c01ae1af9b85",
+  measurementId: "G-T750SFFCCJ"
 };
+
