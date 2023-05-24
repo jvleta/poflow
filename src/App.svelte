c5c9ccaca19559faf863a4ccb12494b5ac8f2640
Jimmy Leta <jimmy.leta@gmail.com>
2023-05-23 22:32:24 -0400

    Committing changes related to initial refactoring effort. The goal is to reuse as much of the existing code as possible to plot the mesh.

--- src/App.svelte
+++ src/App.svelte
@@ -3,46 +3,49 @@
   import { onMount } from "svelte";
   import Plotly from "plotly.js-dist";
 
-  let nacaId = "0012"; // Default NACA ID
+  let nacaId = "0012";
+  let numElements = 10;
   let coordinates = [];
 
-  const nacaIds = [
-    "0012",
-    "0014",
-    "0016" // Sample NACA IDs
-    // Add more NACA IDs as needed
-  ];
+  const nacaIds = ["0012", "0014", "0016"];
+
+  const numElementOptions = [10, 12, 16, 18, 20];
+
+  const linspace = (start, stop, num, endpoint = true) => {
+    const div = endpoint ? num - 1 : num;
+    const step = (stop - start) / div;
+    return Array.from({ length: num }, (_, i) => start + step * i);
+  };
+
+  const calculateNacaFourSeriesYCoordinate = (thicknessToLengthRaio, x) => {
+    return (
+      5.0 *
+      thicknessToLengthRaio *
+      (0.2969 * Math.sqrt(x) -
+        0.126 * x -
+        0.3516 * Math.pow(x, 2.0) +
+        0.2843 * Math.pow(x, 3.0) -
+        0.1015 * Math.pow(x, 4.0))
+    );
+  };
 
   const calculateCoordinates = () => {
-    const thicknessToLengthRaio =
-      parseFloat(nacaId.substring(nacaId.length - 2)) / 100.0;
+    const ratio = parseFloat(nacaId.substring(nacaId.length - 2)) / 100.0;
 
-    coordinates = []; // Clear previous coordinates
-    for (let x = 0; x <= 1; x += 0.001) {
-      const y =
-        5.0 *
-        thicknessToLengthRaio *
-        (0.2969 * Math.sqrt(x) -
-          0.126 * x -
-          0.3516 * Math.pow(x, 2.0) +
-          0.2843 * Math.pow(x, 3.0) -
-          0.1015 * Math.pow(x, 4.0));
+    const xgrid = linspace(0.0, 1.0, 1000);
+
+    coordinates = [];
+    xgrid.forEach((x) => {
+      const y = calculateNacaFourSeriesYCoordinate(ratio, x);
       coordinates.push({ x, y });
-    }
+    });
 
-    for (let x = 0; x <= 1; x += 0.001) {
-      const y =
-        -5.0 *
-        thicknessToLengthRaio *
-        (0.2969 * Math.sqrt(x) -
-          0.126 * x -
-          0.3516 * Math.pow(x, 2.0) +
-          0.2843 * Math.pow(x, 3.0) -
-          0.1015 * Math.pow(x, 4.0));
+    coordinates.forEach((coord) => {
+      const x = coord.x;
+      const y = -coord.y;
       coordinates.push({ x, y });
-    }
+    });
 
-    // Plot the coordinates using Plotly
     const plotData = {
       x: coordinates.map((coord) => coord.x),
       y: coordinates.map((coord) => coord.y),
@@ -55,7 +58,7 @@
       title: `Coordinates for NACA ${nacaId}`,
       xaxis: { title: "X" },
       yaxis: { title: "Y", range: [-0.2, 0.2] },
-      mode: 'lines'
+      mode: "lines",
     };
 
     const plotConfig = {
@@ -66,12 +69,14 @@
   };
 
   onMount(() => {
-    calculateCoordinates(); // Initial coordinate calculation
+    calculateCoordinates();
   });
 
   const handleNacaIdChange = () => {
-    calculateCoordinates(); // Recalculate coordinates on NACA ID change
+    calculateCoordinates();
   };
+
+  const handleNumElementsChange = () => {};
 </script>
 
 <main>
@@ -81,6 +86,16 @@
   <select id="naca-id" bind:value={nacaId} on:change={handleNacaIdChange}>
     {#each nacaIds as id}
       <option value={id}>{id}</option>
+    {/each}
+  </select>
+  <label for="num-elements">Select number of elements:</label>
+  <select
+    id="num-elements"
+    bind:value={numElements}
+    on:change={handleNumElementsChange}
+  >
+    {#each numElementOptions as numElementOption}
+      <option value={numElementOption}>{numElementOption}</option>
     {/each}
   </select>
 
@@ -94,5 +109,24 @@
     width: 600px;
     height: 400px;
     margin: 20px auto;
+  }
+
+  label,
+  select {
+    /* In order to define widths */
+    display: inline-block;
+  }
+
+  label {
+    width: 30%;
+    /* Positions the label text beside the input */
+    text-align: right;
+  }
+
+  label + select {
+    width: 30%;
+    /* Large margin-right to force the next element to the new-line
+           and margin-left to create a gutter between the label and input */
+    margin: 0 30% 0 4%;
   }
 </style>
